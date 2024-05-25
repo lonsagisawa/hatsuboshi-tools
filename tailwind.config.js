36c2aa311c9228f743764dd2f72ecc96a01176b8
Lon Sagisawa <lon@sagisawa.me>
2024-05-25 18:16:06 +0900

    daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
 
