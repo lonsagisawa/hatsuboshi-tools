19967caa649f88d9ecb0cd30b210c0d1acd49324
Lon Sagisawa <lon@sagisawa.me>
2024-05-25 19:18:42 +0900

    style fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <p class="opacity-80">初星学園での生活に、少しだけ使えるかもしれないアプリケーションを提供しています</p>
+  <p class="opacity-80">初星学園での生活に、少しだけ使えるかもしれないWebアプリケーションを<br>提供します</p>
 
   <ul class="my-2">
     <li>
-      <NuxtLink to="/power_calculator" class="underline hover:text-orange-500">最終プロデュース評価計算機</NuxtLink>
+      <NuxtLink to="/power_calculator" class="underline font-medium hover:text-orange-500">最終プロデュース評価計算機</NuxtLink>
     </li>
   </ul>
 </template>
