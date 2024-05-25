1e53d6fdede974ef819789552044062d01a39518
Lon Sagisawa <lon@sagisawa.me>
2024-05-25 18:14:14 +0900

    tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   nitro: {
     prerender: {
       autoSubfolderIndex: false
     },
   },
+
+  modules: ["@nuxtjs/tailwindcss"]
 })
