af28a4b8ec27f742991ec8c451e92f36b4a694f7
Lon Sagisawa <lon@sagisawa.me>
2024-05-25 19:05:17 +0900

    add some styles

--- components/header.vue
+++ components/header.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="px-8 py-4">
+  <header class="p-8 pb-4">
     <h1 class="text-2xl font-bold">
-      <NuxtLink to="/">Hatsuboshi Tools</NuxtLink>
+      <NuxtLink to="/" class="hover:underline">Hatsuboshi Tools</NuxtLink>
     </h1>
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <NuxtLink to="/power_calculator" class="underline hover:text-orange-500">最終プロデュース評価計算機</NuxtLink>
+  <p class="opacity-80">初星学園での生活に、少しだけ使えるかもしれないアプリケーションを提供しています</p>
+
+  <ul class="my-2">
+    <li>
+      <NuxtLink to="/power_calculator" class="underline hover:text-orange-500">最終プロデュース評価計算機</NuxtLink>
+    </li>
+  </ul>
 </template>
