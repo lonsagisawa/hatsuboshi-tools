28a56fd16f7abaeabfb2046d1c8cd79a405eaed6
Lon Sagisawa <lon@sagisawa.me>
2024-06-13 02:21:16 +0900

    Nuxt 4 compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
+  future: {
+    compatibilityVersion: 4,
+  },
   nitro: {
     prerender: {
       autoSubfolderIndex: false
     },
   },
-
   modules: ["@nuxtjs/tailwindcss"]
 })
